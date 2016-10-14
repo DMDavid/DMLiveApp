08b92ad0372aede6de531be5aa04c84613547e2b
David <894566327@qq.com>
2016-10-14 11:43:37 +0800

    init

---
+++ DMLiveApp/DMLiveApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DMLiveApp
+//
+//  Created by 维信金科 on 16/10/11.
+//  Copyright © 2016年 OrangeCat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
